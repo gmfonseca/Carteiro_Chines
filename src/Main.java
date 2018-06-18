bfa008486c3f19e9505bc6aafb0293706ec60d20
gmfonseca <gmagnofonseca@gmail.com>
2018-06-18 15:13:41 -0300

    codigo completo

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    static public void main(String args[]) {
+        CPP G = new CPP(4); // create a graph of four vertices
+
+        // add the arcs for the example graph
+        G.addArc("a", 0, 1, 1).addArc("b", 0, 2, 1).addArc("c", 1, 2, 1)
+         .addArc("d", 1, 3, 1).addArc("e", 2, 3, 1).addArc("f", 3, 0, 1);
+
+        G.solve(); // find the CPT
+        G.printCPT(0); // print it, starting from vertex 0
+        System.out.println("Cost = " + G.cost());
+        OpenCPP.test();
+    }
+
+}
---
+++ src/OpenCPP.java
@@ -0,0 +1,87 @@
+import java.util.Vector;
+
+public class OpenCPP {
+    class Arc {
+        String lab;
+        int u, v;
+        float cost;
+
+        Arc(String lab, int u, int v, float cost) {
+            this.lab = lab;
+            this.u = u;
+            this.v = v;
+            this.cost = cost;
+        }
+    }
+
+    Vector arcs = new Vector();
+    int N;
+
+    OpenCPP(int vertices) {
+        N = vertices;
+    }
+
+    public OpenCPP addArc(String lab, int u, int v, float cost) {
+        if (cost < 0) throw new Error("Graph has negative costs");
+        arcs.addElement(new Arc(lab, u, v, cost));
+        return this;
+    }
+
+    float printCPT(int startVertex) {
+        CPP bestGraph = null, g;
+        float bestCost = 0, cost;
+        int i = 0;
+        do {
+            g = new CPP(N + 1);
+            for (int j = 0; j < arcs.size(); j++) {
+                Arc it = (Arc) arcs.elementAt(j);
+                g.addArc(it.lab, it.u, it.v, it.cost);
+            }
+            cost = g.basicCost;
+            g.findUnbalanced(); // initialise g.neg on original graph
+            g.addArc("'virtual start'", N, startVertex, cost);
+            g.addArc("'virtual end'",
+                    // graph is Eulerian if neg.length=0
+                    g.neg.length == 0 ? startVertex : g.neg[i], N, cost);
+            g.solve();
+            if (bestGraph == null || bestCost > g.cost()) {
+                bestCost = g.cost();
+                bestGraph = g;
+            }
+        } while (++i < g.neg.length);
+        System.out.println("Open CPT from " + startVertex + " (ignore virtual arcs)");
+        bestGraph.printCPT(N);
+        return cost + bestGraph.phi();
+    }
+
+    static void test() {
+        OpenCPP G = new OpenCPP(4); // create a graph of four vertices
+        // add the arcs for the example graph
+        G.addArc("a", 0, 1, 1).addArc("b", 0, 2, 1).addArc("c", 1, 2, 1)
+                .addArc("d", 1, 3, 1).addArc("e", 2, 3, 1).addArc("f", 3, 0, 1);
+        int besti = 0, worsti = 0;
+        float bestCost = 0, worstCost = 0;
+        for (int i = 0; i < 4; i++) {
+            System.out.println("\nSolve from " + i);
+            float c = G.printCPT(i);
+            System.out.println("Cost = " + c + "\n");
+            if (i == 0 || c < bestCost) {
+                bestCost = c;
+                besti = i;
+            }
+            if(i == 0 || c > worstCost){
+                worstCost = c;
+                worsti = i;
+            }
+        }
+
+        System.out.println("\n* FASTEST WAY *");
+        G.printCPT(besti);
+        System.out.println("Cost = " + bestCost);
+
+        System.out.println("\n* SLOWEST WAY *");
+        G.printCPT(worsti);
+        System.out.println("Cost = " + worstCost);
+    }
+
+}
