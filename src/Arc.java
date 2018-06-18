1bb78fd3b8c830ce7db4e3e6b59a60f314725c78
gmfonseca <gmagnofonseca@gmail.com>
2018-06-18 15:17:06 -0300

    todas as classes separadas

---
+++ src/Arc.java
@@ -0,0 +1,12 @@
+class Arc {
+    String lab;
+    int u, v;
+    float cost;
+
+    Arc(String lab, int u, int v, float cost) {
+        this.lab = lab;
+        this.u = u;
+        this.v = v;
+        this.cost = cost;
+    }
+}
